ae7ca1523a633d77062e5399bde766dfc97279a9
MyViboon <95564348+MyViboon@users.noreply.github.com>
2022-11-17 16:51:15 +0700

    add

---
+++ client/src/store/userSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: [],
+}
+
+export const userSlice = createSlice({
+  name: "userStore",
+  initialState: initialState,
+  reducers: {
+    login: (state,action) => {
+      state.user = action.payload
+    },
+    logout: (state) => {
+      //
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
